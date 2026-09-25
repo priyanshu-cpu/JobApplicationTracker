6d1638ebfb8f95821c9d3c9785e9e935b8776f0b
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-25 14:10:08 +0530

    bug fix

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,11 @@
 from fastapi import APIRouter
+from app.schemas.user import User, UserOut
+from app.database import get_db
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/auth")
 
+router.post("register")
+async def create_user(form_data: User, db:Session = Depends(get_db)):
+    pass
--- app/utils/security.py
+++ app/utils/security.py
@@ -30,9 +30,9 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def create_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update{
+    to_encode.update({
         "exp" : expire
-    }
+    })
     token = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
     return token
 
