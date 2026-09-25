bd2276cec5aa9d96a199fbc1e5470a00008c3f79
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-25 13:56:49 +0530

    get current user

---
+++ app/dependencies/auth.py
@@ -0,0 +1,17 @@
+from app.database import get_db
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException
+from app.utils.security import verify_token, credentials_exception
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+
+def get_current_user(payload: dict = Depends(verify_token), db:Session = Depends(get_db)):
+    try:
+        user_id = payload["sub"]
+    except (TypeError, ValueError, KeyError):
+        raise credentials_exception
+    user = db.get(User, user_id)
+    if user is None:
+        raise credentials_exception
+    return user
